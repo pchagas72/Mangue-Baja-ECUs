d811f1674da8db716aaf8d643166bd635c51e616
Pedro Chagas Silva <pedro.pcsilva@ufpe.br>
2025-11-25 23:17:14 -0300

    Updated code for better readability

--- datalogging/temperatura_habitaculo/main/temperatura_habitaculo.c
+++ datalogging/temperatura_habitaculo/main/temperatura_habitaculo.c
@@ -5,13 +5,13 @@
 
 static const char *TAG = "LOGGER";
 
-// DHT
+// DHT definitions
 #define DHT_GPIO_PIN 4
 #define SENSOR_TYPE DHT_TYPE_DHT11 
 
 void app_main(void) {
    
-    // FIX 1: Corrected function call
+    // Initializes sd card
     initialize_sd(TAG);
 
     float temperature = 0;
@@ -20,8 +20,6 @@ void app_main(void) {
     while (1) {
         if (dht_read_float_data(SENSOR_TYPE, DHT_GPIO_PIN, &humidity, &temperature) == ESP_OK) {
             ESP_LOGI(TAG, "Read: Hum: %.1f%% Temp: %.1fC", humidity, temperature);
-            
-            // FIX 2: Added missing 'TAG' argument to match definition
             write_DHT11_to_sd(TAG, temperature, humidity);
         } else {
             ESP_LOGE(TAG, "Could not read data from sensor");
